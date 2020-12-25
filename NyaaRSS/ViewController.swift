b814b5e8f5a77f0b1aebb71900de1cb495c82fd2
kyano <shinkyano2@gmail.com>
2020-12-25 15:47:59 +0100

    Initial Commit

---
+++ NyaaRSS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NyaaRSS
+//
+//  Created by Hervé PIERRE on 25/12/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
